5924a8c846df73fd125daecae025b31f37285773
Ronald van Eede <rveede@tricode.nl>
2012-10-09 13:34:54 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trivote::Application.config.secret_token = 'de6c4b58c53c9ebef5c3bd1b7193929b65aaf7e55245a29f1d94ab45768bd5507127698895f25581ee1d2d98783d9f08384b67e8ea65b9fd4c5ca1181d7ffcf0'
